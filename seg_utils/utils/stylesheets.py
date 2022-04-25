d8671a4c500103cc62ce1145a4d492a5587c3fc7
jakobhampel <jakob.hampel@student.uni-tuebingen.de>
2022-04-25 18:08:34 +0200

    list widgets rebuilt

---
+++ seg_utils/utils/stylesheets.py
@@ -0,0 +1,8 @@
+COMMENT_LIST = """QListWidget {
+                  color: rgb(0, 102, 204);
+                  selection-color: blue;
+                  selection-background-color: white;
+                  }
+                  QListWidget::item:hover {
+                  color: blue;
+                  } """
