2c002b020f465601998c43692d357cbdc0f7907f
jakobhampel <jakob.hampel@student.uni-tuebingen.de>
2022-04-28 21:39:12 +0200

    dialogs rebuilt

--- seg_utils/test/testing.py
+++ seg_utils/test/testing.py
@@ -1,52 +1,42 @@
 """This file's purpose is to import various classes from seg_utils
 in order to test them in isolation"""
-
-from PyQt5.QtWidgets import QApplication
 import sys
 
 from seg_utils.ui.main_window_new import LabelingMainWindow
-from seg_utils.ui.dialogs_new import NewLabelDialog
+from seg_utils.ui.dialogs_new import *
 from seg_utils.ui.list_widgets_new import *
 from seg_utils.utils.qt import colormap_rgb
 
 
-def test_main_window():
-    window = LabelingMainWindow()
-    window.show()
-    app.exec_()
-
-
-def test_new_label_dialog():
-    window = NewLabelDialog()
-    window.show()
-    app.exec()
-
-
-def test_label_list():
-    label_list = LabelList()
-    colors, _ = colormap_rgb(25)
-    classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
-    label_list.update_with_classes(classes, colors)
-    label_list.show()
-    app.exec()
-
-
 def test_comment_list():
+    # done
     comment_list = CommentList()
     comment_list.show()
     app.exec()
 
 
-def test_label_viewing_widget():
-    label_widget = LabelsViewingWidget()
-    colors, _i = colormap_rgb(25)
+def test_dialog_delete_shape():
+    # done
+    dlg = DeleteShapeMessageBox("tumour")
+    print(dlg.answer)
+
+
+def test_dialog_forgot_to_save():
+    # done
+    dlg = ForgotToSaveMessageBox()
+    dlg.exec()
+
+
+def test_dialog_new_label():
+    # done
+    colors, _ = colormap_rgb(25)
     classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
-    label_widget.label_list.update_with_classes(classes, colors)
-    label_widget.show()
-    app.exec()
+    dlg = NewLabelDialog(classes, colors)
+    dlg.exec()
 
 
 def test_file_viewing_widget():
+    # done
     file_widget = FileViewingWidget()
     filenames = ["Picture1", "Picture2", "Video1", "Vid2", "SpecialFile"]
     for fn in filenames:
@@ -56,12 +46,40 @@ def test_file_viewing_widget():
     app.exec()
 
 
+def test_label_list():
+    # done
+    label_list = LabelList()
+    colors, _ = colormap_rgb(25)
+    classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
+    label_list.update_with_classes(classes, colors)
+    label_list.show()
+    app.exec()
+
+
+def test_label_viewing_widget():
+    # done
+    label_widget = LabelsViewingWidget()
+    colors, _i = colormap_rgb(25)
+    classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
+    label_widget.label_list.update_with_classes(classes, colors)
+    label_widget.show()
+    app.exec()
+
+
+def test_main_window():
+    window = LabelingMainWindow()
+    window.show()
+    app.exec_()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    test_main_window()
-    test_new_label_dialog()
-    test_label_list()
-    test_comment_list()
-    test_label_viewing_widget()
-    test_file_viewing_widget()
+    # test_main_window()
+    test_dialog_new_label()
+    test_dialog_delete_shape()
+    test_dialog_forgot_to_save()
+    # test_label_list()
+    # test_comment_list()
+    # test_label_viewing_widget()
+    # test_file_viewing_widget()
--- seg_utils/ui/dialogs_new.py
+++ seg_utils/ui/dialogs_new.py
@@ -1,13 +1,52 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QSize, QPoint, Qt
-from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtGui import QFont, QIcon, QColor
+
+from typing import List
 
 from seg_utils.ui.list_widgets_new import LabelList
+from seg_utils.utils.qt import get_icon
+
+
+class DeleteShapeMessageBox(QMessageBox):
+    def __init__(self, shape: str, *args):
+        super(DeleteShapeMessageBox, self).__init__(*args)
+        if self.parentWidget():
+            move_to_center(self, self.parentWidget().pos(), self.parentWidget().size())
+        reply = self.question(self, "Deleting Shape", f"You are about to delete {shape}. Continue?",
+                              QMessageBox.Yes | QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            self.answer = 1
+        else:
+            self.answer = 0
+
+
+class ForgotToSaveMessageBox(QMessageBox):
+    def __init__(self, *args):
+        super(ForgotToSaveMessageBox, self).__init__(*args)
+
+        save_button = QPushButton(get_icon('save'), "Save Changes")
+        dismiss_button = QPushButton(self.style().standardIcon(QStyle.SP_DialogDiscardButton), "Dismiss Changes")
+        cancel_button = QPushButton(self.style().standardIcon(QStyle.SP_DialogCancelButton), "Cancel")
+
+        self.setWindowTitle("Caution: Unsaved Changes")
+        self.setText("Unsaved Changes: How do you want to progress?")
+
+        # NOTE FOR SOME REASON THE ORDER IS IMPORTANT DESPITE THE ROLE - IDK WHY
+        self.addButton(save_button, QMessageBox.AcceptRole)
+        self.addButton(cancel_button, QMessageBox.RejectRole)
+        self.addButton(dismiss_button, QMessageBox.DestructiveRole)
+
+        if self.parentWidget():
+            move_to_center(self, self.parentWidget().pos(), self.parentWidget().size())
 
 
 class NewLabelDialog(QDialog):
-    def __init__(self, *args):
+    """ a dialog to display all existing label classes so that the user can decide on one
+    user can also create new label classes"""
+    def __init__(self, classes: List[str], color_map: List[QColor], *args):
         super(NewLabelDialog, self).__init__(*args)
+        self.result = ""
 
         self.setFixedSize(QSize(300, 400))
         self.setWindowTitle("Select class of new shape")
@@ -20,43 +59,66 @@ class NewLabelDialog(QDialog):
         self.shape_input.setPlaceholderText("Enter shape label")
         self.shape_input.setMaximumHeight(25)
 
-        self.class_name = ""
+        # info label for when user is about to create a new class
+        self.info = QLabel()
+        self.info.setContentsMargins(0, 0, 0, 0)
+        self.info.setFrameShape(QFrame.NoFrame)
+        self.info.setStyleSheet("color: red")
+        self.info.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # list widget with the already existing classes
         self.label_list = LabelList()
+        self.label_list.update_with_classes(classes, color_map)
         self.confirm = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
 
         # connect
         self.shape_input.textChanged.connect(self.handle_shape_input)
-        self.confirm.accepted.connect(self.close)
+        self.confirm.accepted.connect(self.on_close)
         self.confirm.rejected.connect(self.on_cancel)
         self.label_list.itemClicked.connect(self.on_list_selection)
 
         self.layout().addWidget(self.shape_input)
+        self.layout().addWidget(self.info)
         self.layout().addWidget(self.label_list)
         self.layout().addWidget(self.confirm)
 
     def handle_shape_input(self):
         """ function to filter the list according to user input """
+        self.info.clear()
+        self.result = ""
         text = self.shape_input.text()
+        matches = 0
 
         # iterate through list, display only matches
         for item_idx in range(self.label_list.count()):
-            item = self.listWidget.item(item_idx)
+            item = self.label_list.item(item_idx)
             if text not in item.text():
                 item.setHidden(True)
             else:
                 item.setHidden(False)
+                matches += 1
+
+        # option to create a new label class
+        if matches == 0:
+            self.info.setText("Create new label: {}".format(text))
+            self.result = text
 
     def on_cancel(self):
         """ ensures that the result is an empty string"""
-        self.class_name = ""
+        self.result = ""
         self.close()
+
+    def on_close(self):
+        """ prevents an empty result when user clicks Ok"""
+        if self.result:
+            self.close()
+        else:
+            self.info.setText("Please select or create a label class")
 
     def on_list_selection(self, item: QListWidgetItem):
         """ assigns the item name to the class variable"""
-        self.class_name = item.text()
-
-
-
+        self.info.clear()
+        self.result = item.text()
 
 
 def move_to_center(widget, parent_pos: QPoint, parent_size: QSize):
