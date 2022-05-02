328eaa9f54a0b702529bf1bc93e0568d2d37be6a
jakobhampel <jakob.hampel@student.uni-tuebingen.de>
2022-05-02 14:25:33 +0200

    polyframe rebuilt

--- seg_utils/test/testing.py
+++ seg_utils/test/testing.py
@@ -5,7 +5,17 @@ import sys
 from seg_utils.ui.main_window_new import LabelingMainWindow
 from seg_utils.ui.dialogs_new import *
 from seg_utils.ui.list_widgets_new import *
+from seg_utils.ui.image_display import ImageDisplay
+from seg_utils.ui.poly_frame import PolyFrame
+from seg_utils.ui.shape import Shape
 from seg_utils.utils.qt import colormap_rgb
+
+
+COLORS, _ = colormap_rgb(25)
+CLASSES = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
+SHAPES = [Shape(QSize(10, 10), _class, color=_color, shape_type='polygon')
+          for _class, _color in zip(CLASSES, COLORS)]
+PATIENTS = ["Alex", "Mark", "Clara"]
 
 
 def test_comment_list():
@@ -51,9 +61,7 @@ def test_dialog_forgot_to_save():
 
 def test_dialog_new_label():
     # done
-    colors, _ = colormap_rgb(25)
-    classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
-    dlg = NewLabelDialog(classes, colors)
+    dlg = NewLabelDialog(CLASSES, COLORS)
     dlg.exec()
     print(dlg.result)
 
@@ -70,8 +78,7 @@ def test_dialog_project_handler():
 
 def test_dialog_select_patient():
     # done
-    existing_patients = ["Alex", "Mark", "Clara"]
-    dlg = SelectPatientDialog(existing_patients)
+    dlg = SelectPatientDialog(PATIENTS)
     dlg.exec()
     print(dlg.result)
 
@@ -87,12 +94,18 @@ def test_file_viewing_widget():
     app.exec()
 
 
+def test_image_display():
+    window = ImageDisplay()
+    pm = QPixmap("../examples/images/video0001_0001.png")
+    window.init_image(pm, None)
+    window.show()
+    app.exec()
+
+
 def test_label_list():
     # done
     label_list = LabelList()
-    colors, _ = colormap_rgb(25)
-    classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
-    label_list.update_with_classes(classes, colors)
+    label_list.update_with_classes(CLASSES, COLORS)
     label_list.show()
     app.exec()
 
@@ -100,9 +113,7 @@ def test_label_list():
 def test_label_viewing_widget():
     # done
     label_widget = LabelsViewingWidget()
-    colors, _i = colormap_rgb(25)
-    classes = ["Tumour", "Blood", "Vein", "Healthy Tissue"]
-    label_widget.label_list.update_with_classes(classes, colors)
+    label_widget.label_list.update_with_classes(CLASSES, COLORS)
     label_widget.show()
     app.exec()
 
@@ -113,10 +124,17 @@ def test_main_window():
     app.exec_()
 
 
+def test_poly_frame():
+    window = PolyFrame()
+    window.update_frame(SHAPES)
+    window.show()
+    app.exec()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # test_main_window()
+    test_main_window()
     # test_dialog_new_label()
     # test_dialog_delete_shape()
     # test_dialog_forgot_to_save()
@@ -124,8 +142,10 @@ if __name__ == "__main__":
     # test_dialog_delete_class()
     # test_dialog_select_patient()
     # test_dialog_project_handler()
-    test_dialog_comment()
+    # test_dialog_comment()
     # test_label_list()
     # test_comment_list()
     # test_label_viewing_widget()
     # test_file_viewing_widget()
+    # test_image_display()
+    test_poly_frame()
